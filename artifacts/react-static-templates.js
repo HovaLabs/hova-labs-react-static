377d6cb42cd652b10efae95b696db6fd1300b3ee
matthova <matthova87@gmail.com>
2019-12-26 19:58:43 -0800

    Initial commit

---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/404.tsx': require('/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/404.tsx').default,
+'/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/about.tsx': require('/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/about.tsx').default,
+'/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/blog.tsx': require('/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/blog.tsx').default,
+'/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/index.tsx': require('/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/index.tsx').default,
+'/Users/matthewhovanec/Sites/hova-labs-react-static/src/containers/Post': require('/Users/matthewhovanec/Sites/hova-labs-react-static/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/matthewhovanec/Sites/hova-labs-react-static/src/pages/404.tsx'
+
