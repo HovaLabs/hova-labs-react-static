b64639704a0ee9a761e1cd57e6843a0934de9ba6
matthova <matthova87@gmail.com>
2020-01-03 12:57:11 -0800

    added image example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
 import React from "react";
+import { createStyledComponent } from "@hova-labs/bento-box-web";
+import hovalin from "../media/hovalin-64.jpg";
 
-export default () => <div>Hova Labs</div>;
+const HeroWrapper = createStyledComponent("div")({
+  width: "100%",
+  overflow: "hidden",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+});
+
+const Hero = createStyledComponent("img")({
+  width: "100%",
+  minWidth: 600,
+  backgroundPosition: "center center",
+  backgroundRepeat: "no-repeat"
+});
+
+export default () => (
+  <div>
+    <HeroWrapper>
+      <Hero src={hovalin} />
+    </HeroWrapper>
+    <div>Hova Labs</div>
+  </div>
+);
