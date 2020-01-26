e791597ad19d0cb5f1e039d3bf16fd5da7566c8a
Kaitlyn Hova <kaitlynhova@gmail.com>
2020-01-25 21:37:11 -0800

    almost fixing about

--- src/pages/HomePage/About.tsx
+++ src/pages/HomePage/About.tsx
@@ -5,7 +5,7 @@ import { Text } from "@hova-labs/bento-box-web";
 import theHovas from "../../media/the-hovas.png";
 
 // ABOUT START
-const AboutContainer = styled("div")`
+const OuterContainer = styled("div")`
   ${p =>
     p.theme.responsiveStyle("margin-top", {
       s: `${p.theme.spacings.l}px`,
@@ -14,65 +14,56 @@ const AboutContainer = styled("div")`
   background-color: ${p => p.theme.colors.backgroundSecondary};
 `;
 
-const AboutImageContainer = styled("div")`
-  width: 100%;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const AboutImageFiller = styled("div")`
-  flex: 1;
-`;
-
-const AboutImage = styled("img").attrs({ src: theHovas })`
-  width: 100%;
-  flex: 1;
-  min-width: 500px;
-  background-position: center center;
+const InnerContainer = styled("div")`
+  background-image: url(${theHovas});
+  ${p =>
+    p.theme.responsiveStyle("display", {
+      s: `block`,
+      l: "flex",
+    })}
+  max-width: 1200px;
+  background-size: contain;
   background-repeat: no-repeat;
+  ${p =>
+    p.theme.responsiveStyle("background-position", {
+      s: `bottom center`,
+      l: `bottom right`,
+    })}
+  ${p =>
+    p.theme.responsiveStyle("padding-bottom", {
+      s: `300px`,
+      l: "0",
+    })}
 `;
 
 const AboutTextContainer = styled("div")`
-  ${p =>
-    p.theme.responsiveStyle("position", {
-      s: "relative",
-      l: "absolute",
-    })}
   ${p =>
     p.theme.responsiveStyle("padding", {
       s: `${p.theme.spacings.l}px`,
       l: `${p.theme.spacings.xxl}px`,
     })}
-  padding-bottom: 0;
-  ${p =>
-    p.theme.responsiveStyle("width", {
-      s: `100%`,
-      l: "50%",
-    })}
+  max-width: 600px;
 `;
 // ABOUT END
 
 export const About = () => (
-  <AboutContainer>
-    <AboutTextContainer>
-      <div>
-        <Text typography="headingSmall">ABOUT</Text>
-      </div>
-      <div>
-        <Text typography="headingLarge">The Hovas</Text>
-      </div>
-      <div>
-        <Text>
-          We are two Nebraska nerds currently living in the SF Bay Area who use
-          diverse backgrounds in art and science to buld scalable projects.
-        </Text>
-      </div>
-    </AboutTextContainer>
-    <AboutImageContainer>
-      <AboutImageFiller />
-      <AboutImage />
-    </AboutImageContainer>
-  </AboutContainer>
+  <OuterContainer>
+    <InnerContainer>
+      <AboutTextContainer>
+        <div>
+          <Text typography="headingSmall">ABOUT</Text>
+        </div>
+        <div>
+          <Text typography="headingLarge">The Hovas</Text>
+        </div>
+        <div>
+          <Text>
+            We are two Nebraska nerds currently living in the SF Bay Area who
+            use diverse backgrounds in art and science to buld scalable
+            projects.
+          </Text>
+        </div>
+      </AboutTextContainer>
+    </InnerContainer>
+  </OuterContainer>
 );
