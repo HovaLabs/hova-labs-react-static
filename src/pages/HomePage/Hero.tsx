ab901b13612cef2b8d184b315b542ff717c5da26
matthova <matthova87@gmail.com>
2020-02-02 00:10:21 -0800

    Patch hero text

--- src/pages/HomePage/Hero.tsx
+++ src/pages/HomePage/Hero.tsx
@@ -5,6 +5,7 @@ import hovalin from "../../media/hovalin-64.jpg";
 
 // HERO START
 const HeroContainer = styled("div")`
+  position: relative;
   width: 100%;
   overflow: hidden;
   display: flex;
@@ -39,7 +40,7 @@ const HeroImage = styled("img").attrs({ src: hovalin })`
 
 export const Hero = (): React.ReactElement => (
   <HeroContainer>
-    <HeroText>HOVA LABS</HeroText>
     <HeroImage />
+    <HeroText>HOVA LABS</HeroText>
   </HeroContainer>
 );
