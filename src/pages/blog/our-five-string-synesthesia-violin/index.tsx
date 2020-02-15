db20c7351e7db3471065e3486989e98a7c4d5a70
matthova <matthova87@gmail.com>
2020-02-15 07:09:05 -1000

    Added content to synesthesia violin blog

---
+++ src/pages/blog/our-five-string-synesthesia-violin/index.tsx
@@ -0,0 +1,104 @@
+import React from "react";
+import violinPhoto1 from "./violin-photo-1.jpg";
+import violinPhoto2 from "./violin-photo-2.jpg";
+import violinPhoto3 from "./violin-photo-3.jpg";
+import violinPhoto4 from "./violin-photo-4.jpg";
+import violinPhoto5 from "./violin-photo-5.jpg";
+import violinPhoto6 from "./violin-photo-6.jpg";
+import violinPhoto7 from "./violin-photo-7.jpg";
+import violinPhoto8 from "./violin-photo-8.jpg";
+import violinPhoto9 from "./violin-photo-9.jpg";
+
+export default (): React.ReactElement => (
+  <div>
+    <div>
+      Since the beginning of our pursuit of 3D printing violins, we've had a
+      very specific list of checkboxes in mind for our "no holds barred",
+      perfect violin.
+    </div>
+    <ul>
+      <li>3D printable on a "Desktop" printer</li>
+      <li>5 String</li>
+      <li>Barbera pickup</li>
+      <li>Translucent with LEDs in it</li>
+      <li>Responsive to sound and pitch</li>
+    </ul>
+    <div>
+      Just checking off the first box was quite an endeavor. We're nowhere near
+      done tweaking our design, but we wereso happy to have our first working
+      prototype of what I call, the"Synesthesia Projector" Hovalin.
+    </div>
+    <div>Here's a clip of the violin in action:</div>
+    <iframe
+      title="hovalin-in-action"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/kvYgzX4rx8U"
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+    <div>Here's a quick walkthrough of the build:</div>
+    <div>
+      We added a slot for a 5-string Barbera pickup, as well as an on/off switch
+      for the electronics.
+    </div>
+    <img src={violinPhoto1} alt="hovalin-pickup" />
+    <div>
+      Below that pickup, (You can sort of see it through the F hole) we have a
+      cheapo piezopickup that will be used to drive the pitch and level
+      detection.
+    </div>
+    <img src={violinPhoto2} alt="hovalin-pickup-alternate-angle" />
+    <div>
+      In order tocharge the LED system, we added a usb-micro extension cable. To
+      mount it inplace, we covered a chopstick in hot glue and proceeded to
+      apply it liberally to the general area.
+    </div>
+    <img src={violinPhoto3} alt="hovalin-hot-glue" />
+    <img src={violinPhoto4} alt="hovalin-charging-port" />
+    <div>
+      All signal processing is done by the Teensy3.6 and the Teensy Audio board.
+      The goal of the Teensy is to convert theinput microphone's pitch into a
+      specific color, and have the volume drivethe brightness.
+    </div>
+    <div>Some interesting bugs I ran into:</div>
+    <ul>
+      <li>
+        If I started to drive morethan ~40 neopixels, I would start to
+        experience ring modulation in my audioprocessing sketch.
+      </li>
+      <li>
+        The noise generated from driving neopixels could bepicked up by the
+        audio circuit. If it picked up enough noise from drivingthe LEDs, it
+        would eventually reach a never-ending noise/light/feedback loopwhere the
+        noise generated from the LEDs would create audio which would makethe
+        LEDs brighter, and so on.
+      </li>
+      <li>
+        The Teensy 3.2 (or perhaps my poor soldering)was more susceptible to
+        noise.
+      </li>
+    </ul>
+    <div>
+      I'd love to improve the sketch, but for now,it's working just fine. My
+      guess is that having a dedicated brain for the neopixels will make things
+      run a lot smoother.
+    </div>
+    <img src={violinPhoto5} alt="hovalin-electronics" />
+    <img src={violinPhoto6} alt="hovalin-power-input" />
+    <div>
+      The violin has a gamut of connections rolling out of it. I didn't get a
+      proper photo, but the Teensy on/off switch works by breaking a USB cable's
+      5V (red wire).
+    </div>
+    <img src={violinPhoto7} alt="hovalin-power-enclosure" />
+    <div>Last, but not least, we painted in the H logo on the Hovalin.</div>
+    <img src={violinPhoto8} alt="hovalin-logo" />
+    <div>
+      Well, there you have it, our 5 string Synesthesia Projector Hovalin is
+      built and working. Can't wait to post more videos of it in action soon.
+    </div>
+    <img src={violinPhoto9} alt="hovalin-top-view" />
+  </div>
+);
