fa0d10cc17bdd0070a71185a2132911aa200052c
matthova <matthova87@gmail.com>
2020-02-22 10:28:23 -0800

    Docs wip

--- src/pages/hovalin/docs/Docs.tsx
+++ src/pages/hovalin/docs/Docs.tsx
@@ -1,3 +1,271 @@
 import React from "react";
+import { Button } from "../../../components";
 
-export const Docs = (): React.ReactElement => <div>Docs</div>;
+const shoppingList = [
+  {
+    icon: "check",
+    title: "3D PRINTER",
+    description: "Here are some printers that we recommend.",
+    list: [
+      {
+        name: "CR-10S",
+        link:
+          "https://www.gearbest.com/3d-printers-3d-printer-kits/pp_778981.html?wid=1859758&lkid=16008056",
+      },
+      {
+        name: "Raise3D N2 Plus",
+        link:
+          "https://www.amazon.com/gp/product/B01NAYIQZ3/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01NAYIQZ3&linkCode=as2&tag=hovalin-20&linkId=8d696c33dbdadd8521a87545bdaf804e",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "PLASTIC",
+    description:
+      "We like Hatchbox filament because of its quality and low cost, but any filament on Amazon with a 4 star rating from more than 10 people is probably fine.",
+    list: [
+      {
+        name: "Hatchbox 1.75mm Black PLA Filament",
+        link:
+          "https://www.amazon.com/gp/product/B00J0ECR5I/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00J0ECR5I&linkCode=as2&tag=hovalin-20&linkId=d68dfe6b34366d8ac8358034ed944540",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "TUNERS",
+    description:
+      "The Hovalin is designed specifically to these tuner dimensions, so please use them:",
+    list: [
+      {
+        name: "Black",
+        link:
+          "http://www.amazon.com/gp/product/B009AQIZYS/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B009AQIZYS&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=RR43FGQCYG63T4VS",
+      },
+      {
+        name: "Chrome",
+        link:
+          "http://www.amazon.com/gp/product/B00BJTGILG/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B00BJTGILG&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=X4L4INPTZ2DNTHGO",
+      },
+      {
+        name: "Gold",
+        link:
+          "http://www.amazon.com/gp/product/B0092M2EZQ/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0092M2EZQ&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=QK5MVIX45BJKMD5F",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "ROSIN",
+    description:
+      "Rosin helps to keep your violin from squeaking and keeps the bow on the strings.Here are a few brands of rosin that we suggest:",
+    list: [
+      {
+        name: " Bellafina Rosin",
+        link:
+          "http://www.amazon.com/gp/product/B0064RM2IG/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0064RM2IG&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=6IC23B5RPEXCYDLX",
+      },
+      {
+        name: "Leto Rosin",
+        link:
+          "http://www.amazon.com/gp/product/B0087V0L9O/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0087V0L9O&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=NK3T7EMZGCHCCHAK",
+      },
+      {
+        name: "Bernardel Rosin",
+        link:
+          "http://www.amazon.com/gp/product/B000F3HK78/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B000F3HK78&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=JVF4UE6OTTAHZDPY",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "STRINGS",
+    description:
+      "Strings can be expensive. We are fans of high value, low cost.Here are a few brands of strings that we endorse:",
+    list: [
+      {
+        name: "D'Addario Prelude",
+        link:
+          "http://www.amazon.com/gp/product/B0002Y6BJI/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0002Y6BJI&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=RMBKSKN5ZX76LXDL",
+      },
+      {
+        name: "D'Addario Helicore",
+        link:
+          "http://www.amazon.com/gp/product/B0002D08OI/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0002D08OI&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=WBFF4EM3CVW3HIDM",
+      },
+      {
+        name: "Thomastik Reds",
+        link:
+          "http://www.amazon.com/gp/product/B001LRJ6KG/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B001LRJ6KG&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=TBPRSCOJDC5WYJXK",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "CARBON FIBER RODS",
+    description:
+      "8mm (aka 5/16 inch) outer diameter by 500mm length carbon fiber rod. No cutting necessary",
+    list: [
+      {
+        name: "8mm OD x 500mm length carbon fiber rod",
+        link:
+          "https://www.amazon.com/gp/product/B00TF8UY9A/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B00TF8UY9A&linkCode=as2&tag=hovalin-20&linkId=f9f34fbc52611a560fe12e569165050d",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "BOW",
+    description:
+      "Bows can also be expensive, we are fans of high value, low cost.Here are a few brands of bows that we support.",
+    list: [
+      {
+        name: "ViolinSmart Violin Bow",
+        link:
+          "http://www.amazon.com/gp/product/B000VO1EA0/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B000VO1EA0&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=FWQTVZNADTN27EOH",
+      },
+      {
+        name: "Balanced Brazilwood Bow",
+        link:
+          "http://www.amazon.com/gp/product/B00DW12HR8/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B00DW12HR8&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=MT5E5EDF44KDROUF",
+      },
+      {
+        name: "Carbon Fiber Violin Bow",
+        link:
+          "http://www.amazon.com/gp/product/B00FTSOPEK/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B00FTSOPEK&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=SLYIPI5DS74SAZXG",
+      },
+    ],
+  },
+  {
+    icon: "check",
+    title: "EXTRAS",
+    description: "More stuff",
+    list: [
+      {
+        name: "Fishman Pickup",
+        link:
+          "http://www.amazon.com/gp/product/B0002IHFGW/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B0002IHFGW&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=TBDJ2G5KRNCKK5ZB",
+      },
+      {
+        name: "Socket wrench set",
+        link:
+          "http://www.amazon.com/gp/product/B00004TBL1/ref=as_li_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B00004TBL1&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=I5FGWGZEFN5MSXEJ",
+      },
+      {
+        name: "Blue Tape",
+        link:
+          "http://www.amazon.com/gp/product/B00LAJNM7S/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B00LAJNM7S&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=KASDE2Q4HYHUFPTG",
+      },
+      {
+        name: "Print Removal Tools",
+        link:
+          "http://www.amazon.com/gp/product/B01DVYLJ3S/ref=as_li_qf_sp_asin_il_tl?ie=UTF8&amp;camp=1789&amp;creative=9325&amp;creativeASIN=B01DVYLJ3S&amp;linkCode=as2&amp;tag=hovalin-20&amp;linkId=4BOVD54GNA7UMJKK",
+      },
+    ],
+  },
+];
+
+export const Docs = (): React.ReactElement => (
+  <div>
+    <div>
+      <div>Hovalin</div>
+      <div>
+        <Button
+          href="https://s3-us-west-2.amazonaws.com/hovalin/v4.0.0/4_4/Hovalin+v4.0.0+4_4.zip"
+          onPress={() => {}}
+          title="Download v4.0.0 .stl files"
+        />
+        <Button
+          href="https://a360.co/2Oj1DKr"
+          onPress={() => {}}
+          title="Check out the CAD"
+        />
+        <Button
+          href="https://www.reddit.com/r/hovalin_community/"
+          onPress={() => {}}
+          title="Discuss on Reddit"
+        />
+      </div>
+      <div>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://www.hovalabs.com/hova-instruments/hovalin/docs/v3.1.0"
+        >
+          Looking for v3?
+        </a>
+      </div>
+    </div>
+    <div>
+      <a href="#shopping-list">
+        <div>
+          <div>1</div>
+          <div>Shopping List</div>
+        </div>
+      </a>
+      <a href="#print-parts">
+        <div>
+          <div>2</div>
+          <div>Print Parts</div>
+        </div>
+      </a>
+      <a href="#clean-up">
+        <div>
+          <div>3</div>
+          <div>Clean up Prints</div>
+        </div>
+      </a>
+      <a href="#pegs">
+        <div>
+          <div>4</div>
+          <div>Install Pegs</div>
+        </div>
+      </a>
+      <a href="#assemble">
+        <div>
+          <div>5</div>
+          <div>Assemble Body</div>
+        </div>
+      </a>
+      <a href="#strings">
+        <div>
+          <div>6</div>
+          <div>Install Strings</div>
+        </div>
+      </a>
+    </div>
+    <div>
+      <div>warning icon</div>
+      <div>
+        Warning!!! As with any craft, violin making and 3D printing requires
+        attention to safety. Do not attempt to build the violin without parental
+        supervision. When in doubt, ask for assistance.
+      </div>
+    </div>
+    <div>
+      <div>
+        <div>1</div>
+        <div>Shopping List</div>
+      </div>
+      <div>
+        {shoppingList.map(item => (
+          <div>
+            <div>{item.icon}</div>
+            <div>
+              <div>{item.title}</div>
+              <div>{item.description}</div>
+              <div>
+                {item.list.map(li => (
+                  <a href={li.link}>
+                    <div>{li.name}</div>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  </div>
+);
