eb73667be0c71f173d30bf56ee94d5f276d6c6df
matthova <matthova87@gmail.com>
2020-02-22 22:23:50 -0800

    Added hovalin v2 blog

--- src/pages/blog/Blog/blogList.ts
+++ src/pages/blog/Blog/blogList.ts
@@ -1,11 +1,12 @@
 import hovalinThreeOhPlanHeroImage from "../hovalin-three-oh-plan/hero.jpg";
+import hovalinTwoHeroImage from "../hovalin-two/hero.jpg";
 import ourFiveStringSynesthesiaViolinHeroImage from "../our-five-string-synesthesia-violin/hero.jpg";
 
 export const blogList = [
   {
     hero: ourFiveStringSynesthesiaViolinHeroImage,
     datePublished: "2020-02-12T16:26:08.712Z",
-    tags: ["music", "hovalin"],
+    tags: ["electronics", "hovalin"],
     title: "Our 5 String Synesthesia Violin",
     url: "/blog/our-five-string-synesthesia-violin",
   },
@@ -15,5 +16,12 @@ export const blogList = [
     tags: ["hovalin"],
     title: "HOVALIN 3.0 PLAN",
     url: "/blog/hovalin-three-oh-plan",
+  },
+  {
+    hero: hovalinTwoHeroImage,
+    datePublished: "2016-01-06T06:10:44.160Z",
+    tags: ["hovalin"],
+    title: "HOVALIN V2.0 IS LIVE",
+    url: "/blog/hovalin-two",
   },
 ];
---
+++ src/pages/blog/hovalin-two/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default (): React.ReactElement => (
+  <div>
+    <div>
+      tl;dr You have access to the CAD. It's easier to print and assemble.
+    </div>
+
+    <div>Hovalin 2.0, What and Why:</div>
+    <div>
+      Today we're excited to launch Hovalin V2.0! As is the case for any labor
+      of love, the Hovalin project will never truly be "done". That being said,
+      we're now ready to mark the next step in the design's evolution.
+    </div>
+    <div>
+      Over the past few months, I've had a blast redesigning the violin from
+      scratch in Fusion 360. We've learned a lot of new tricks and gotten some
+      pro advice from co-workers and friends. Here are some of the notable
+      improvements that we've added in the latest release:
+    </div>
+    <ul>
+      <li>Easier to insert truss rods</li>
+      <li>
+        The violin body now has a constant thickness (Woot Woot lofts with guide
+        rails)
+      </li>
+      <li>Notch for adding a fishman pickup to the bridge</li>
+      <li>Optimized spacing for tuning pegs</li>
+      <li>Easy to modify neck length</li>
+      <li>The Fusion 360 CAD files are now available</li>
+    </ul>
+    <div>
+      As for the future of the Hovalin, there are still a ton of ways that the
+      design can be improved. Most notably, we're beginning work on 1/2 and 1/4
+      sized models in order to make it possible to introduce the Hovalin into
+      school music programs.
+    </div>
+    <div>...But that's for another blog post</div>
+    <div>
+      Check out the new files. As always, we love feedback. Feel free to send us
+      messages via the contact form. If you want to see a feature added to the
+      Hovalin, please post an issue on The Hovalin's GitHub page.
+    </div>
+    <div>Cheers, and Happy 2016!</div>
+    <iframe
+      title="Hovalin v2.0 video"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/9fZ_vYkuuZU"
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </div>
+);
