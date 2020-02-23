aad3a024b7b5555375ea905bbde73d8010e842b3
matthova <matthova87@gmail.com>
2020-02-22 21:55:35 -0800

    Removed boilerplate blog posts

--- src/pages/blog/foo-the-bar/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { BlogContainer } from "../../../components/BlogContainer";
-
-export default (): React.ReactElement => (
-  <BlogContainer>Foo The BAR!!!</BlogContainer>
-);
--- src/pages/blog/foo-the-foo/index.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default (): React.ReactElement => <div>Foo The FOO!!!</div>;
