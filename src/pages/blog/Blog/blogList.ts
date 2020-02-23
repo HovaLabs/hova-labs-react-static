c03a4fe6858b965474181314ca81b4c45b1519d1
matthova <matthova87@gmail.com>
2020-02-22 22:13:40 -0800

    Added Hovalin 3 blog

--- src/pages/blog/Blog/blogList.ts
+++ src/pages/blog/Blog/blogList.ts
@@ -1,5 +1,4 @@
-import fooTheBarHeroImage from "../foo-the-bar/hero.jpg";
-import fooTheFooHeroImage from "../foo-the-foo/hero.jpg";
+import hovalinThreeOhPlanHeroImage from "../hovalin-three-oh-plan/hero.jpg";
 import ourFiveStringSynesthesiaViolinHeroImage from "../our-five-string-synesthesia-violin/hero.jpg";
 
 export const blogList = [
@@ -11,17 +10,10 @@ export const blogList = [
     url: "/blog/our-five-string-synesthesia-violin",
   },
   {
-    hero: fooTheBarHeroImage,
-    datePublished: "2020-02-10T16:26:08.712Z",
-    tags: ["food"],
-    title: "Foo the Bar",
-    url: "/blog/foo-the-bar",
-  },
-  {
-    hero: fooTheFooHeroImage,
-    datePublished: "2020-02-02T16:26:08.712Z",
-    tags: ["food"],
-    title: "Foo the Foo",
-    url: "/blog/foo-the-foo",
+    hero: hovalinThreeOhPlanHeroImage,
+    datePublished: "2016-04-18T06:10:44.160Z",
+    tags: ["hovalin"],
+    title: "HOVALIN 3.0 PLAN",
+    url: "/blog/hovalin-three-oh-plan",
   },
 ];
---
+++ src/pages/blog/hovalin-three-oh-plan/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+export default (): React.ReactElement => (
+  <div>
+    <div>
+      This post is half update and half an apology for the United States not
+      using the metric system. Enjoy!
+    </div>
+    <br />
+    <div>
+      Thus far in our work with Hovalin, we've spent a lot of time developing a
+      design before talking about or releasing it. This post is a departure from
+      our norm. We want to give you a sneak peak into some problems that we're
+      hoping to create solutions for.
+    </div>
+    <div>Here's the feature-list:</div>
+    <ul>
+      <li>
+        Move away from the current carbon fiber rectangle to an 8mm (5/16") rod.
+        Because the rest of the world outside of the US uses the metric system,
+        our current carbon fiber rods are difficult to source if you are not in
+        the US. Who knew? We have heard you, and we are gettin' to it!
+      </li>
+      <li>
+        Create a "lock" that will lock together the top and bottom pieces with
+        the middle piece.
+      </li>
+      <li>Create custom brims that will reduce warping.</li>
+      <li>
+        Seriously consider actually CAD'ing a chin-rest and shoulder-rest.
+      </li>
+      <li>
+        Work out a new system for distributing multiple options for the .stls
+        including files with brim, files without brim, pre-sliced files with
+        supports for the middle piece.
+      </li>
+    </ul>
+
+    <div>
+      It's quite the list, but we're excited to slowly chip away and make the
+      Hovalin just a bit more easy to manufacture.
+    </div>
+    <div>
+      In case you want to get your hands dirty, here's a link to the cad's work
+      in progress: http://a360.co/1pbwxDH
+    </div>
+    <div>
+      I have a feeling that the rod will create some unwanted interference with
+      the tuning pegs. If we can work through a solution for this, not only will
+      the total bill of materials for a Hovalin lower in cost, it should also
+      make the parts more accessible for the majority of the world that actually
+      respects the metric system.
+    </div>
+    <div>Anyway, Happy Monday.</div>
+    <div>-Matt</div>
+  </div>
+);
