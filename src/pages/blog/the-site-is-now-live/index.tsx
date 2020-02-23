3d9980ee3334865f88a20e135f84c985dd61d178
matthova <matthova87@gmail.com>
2020-02-22 23:47:40 -0800

    done w blogs

---
+++ src/pages/blog/the-site-is-now-live/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default (): React.ReactElement => (
+  <div>
+    <div>Woo!</div>
+    <div>
+      If you're reading this, it means that our site is now live. We've had a
+      lot of fun learning how to work with the blogging platform, ghost. Check
+      it out here.
+    </div>
+    <div>
+      We're looking forward to constantly improving the violin design,
+      documentation, and electronic addons. Most importantly, we're excited to
+      see what YOU do with it. Please share with us any photos, videos, or
+      feedback.
+    </div>
+    <div>All the high fives,</div>
+    <div>Matt + Kaitlyn</div>
+  </div>
+);
