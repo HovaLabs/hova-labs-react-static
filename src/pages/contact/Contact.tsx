ac144841f2e64b479383e3c1398a428211daab3c
matthova <matthova87@gmail.com>
2020-02-29 08:15:30 -0800

    Basic contact form

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,3 +1,10 @@
 import React from "react";
 
-export const Contact = (): React.ReactElement => <div>contact</div>;
+export const Contact = (): React.ReactElement => (
+  <div style={{ marginTop: 120 }}>
+    <div>Contact us to talk about a project or just to say hello!</div>
+    <div>
+      <a href="mailto:hello@hovalabs.com">hello@hovalabs.com</a>
+    </div>
+  </div>
+);
