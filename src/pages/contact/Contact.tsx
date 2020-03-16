a930198dc56eaa21e044b3089faa214c31fbb2b0
Kaitlyn Hova <kaitlynhova@gmail.com>
2020-03-15 21:52:46 -0700

    quick contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Text } from "@hova-labs/bento-box-web";
+import { ContainerWithBorder } from "../../components/Container/ContainerWithBorder";
+import { NavigationFooter } from "../../components/Navigation/NavigationFooter";
+import * as S from "./ContactStyles";
 
 export const Contact: React.FC<{}> = () => (
-  <div>
-    <div>Contact us to talk about a project or just to say hello!</div>
-    <div>
-      <a href="mailto:hello@hovalabs.com">hello@hovalabs.com</a>
-    </div>
-  </div>
+  <>
+    <ContainerWithBorder>
+      <S.OuterContainer>
+        <Text typography="headingLarge">
+          Contact us to talk about a project or just to say hello!
+        </Text>
+        <S.EmailContainer>
+          <a href="mailto:hello@hovalabs.com">
+            <Text typography="headingSmall" color="primary">
+              hello@hovalabs.com
+            </Text>
+          </a>
+        </S.EmailContainer>
+      </S.OuterContainer>
+    </ContainerWithBorder>
+    <NavigationFooter />
+  </>
 );
