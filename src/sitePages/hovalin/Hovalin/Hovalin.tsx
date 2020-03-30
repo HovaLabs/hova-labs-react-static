e90b1eabc965d710520e13d4119d76fa745d8f31
Kaitlyn Hova <kaitlynhova@gmail.com>
2020-03-29 21:12:12 -0700

    chopping up the Hova instruments page into sections

--- src/sitePages/hovalin/Hovalin/Hovalin.tsx
+++ src/sitePages/hovalin/Hovalin/Hovalin.tsx
@@ -7,7 +7,11 @@ import { routes } from "../../../routes";
 import Layout from "../../../components/layout";
 
 import { Button } from "../../../components";
-import { ContainerWithBorder } from "../../../components/Container/ContainerWithBorder";
+import {
+  ContainerWithBorder,
+  ContainerFlex,
+  ContainerContent,
+} from "../../../components/Container";
 import * as S from "./HovalinStyles";
 
 export const Hovalin: React.FC<{}> = () => (
@@ -39,47 +43,64 @@ export const Hovalin: React.FC<{}> = () => (
           />
         </S.ButtonPadding>
       </ContainerWithBorder>
-      <div>
-        <img src={hovalinAbout} alt="hovalin" />
-        <h1>About</h1>
-        <h2>A Violin for Makers</h2>
-        <div>
-          The hovalin is a functional acoustic violin that can be produced using
-          most standard consumer 3d printers. The total cost of raw materials
-          for the violin (4/4) is currently about $70. The hovalin was designed
-          with Autodesk Fusion 360, and its shape and dimensions are inspired by
-          the Stradivarius violin model. The hovalin project is inspired in part
-          by David Perry's FFFiddle. Check it out here. Read our FAQ
-        </div>
-        <Button
-          href={routes.HOVALIN_FAQ}
-          title="Read our FAQ"
-          onPress={() => {}}
-        />
-      </div>
-      <div>
-        <img src={makeHero} alt="making a violin" />
-        <h1>Let's make one</h1>
-        <div>Visit the Hovalin Docs for step by step instructions</div>
-        <Button
-          href={routes.HOVALIN_DOCS}
-          title="Make a Hovalin"
-          onPress={() => {}}
-        />
-      </div>
-      <div>
-        <h2>Listen Up</h2>
-        <h1>But what does it sound like?</h1>
-        <iframe
-          title="Hovalin Preview Video"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/YbcQXEyzreA"
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </div>
+      <ContainerWithBorder>
+        <ContainerFlex>
+          <ContainerContent flexL="50">
+            <img src={hovalinAbout} alt="hovalin" width="50%" />
+          </ContainerContent>
+          <ContainerContent flexL="50">
+            <Text typography="headingMedium">A Violin for Makers</Text>
+            <Text typography="bodyText">
+              The hovalin is a functional acoustic violin that can be produced
+              using most standard consumer 3d printers. The total cost of raw
+              materials for the violin (4/4) is currently about $70. The hovalin
+              was designed with Autodesk Fusion 360, and its shape and
+              dimensions are inspired by the Stradivarius violin model. The
+              hovalin project is inspired in part by David Perry's FFFiddle.
+              Check it out here. Read our FAQ
+            </Text>
+            <Button
+              href={routes.HOVALIN_FAQ}
+              title="Read our FAQ"
+              onPress={() => {}}
+            />
+          </ContainerContent>
+        </ContainerFlex>
+      </ContainerWithBorder>
+      <ContainerWithBorder>
+        <ContainerFlex>
+          <ContainerContent flexL="50">
+            <Text typography="headingMedium">But what does it sound like?</Text>
+          </ContainerContent>
+          <ContainerContent flexL="50">
+            <iframe
+              title="Hovalin Preview Video"
+              width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/YbcQXEyzreA"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </ContainerContent>
+        </ContainerFlex>
+      </ContainerWithBorder>
+      <ContainerWithBorder
+        backgroundImage={makeHero}
+        backgroundImageStyles={{ opacity: 0.2 }}
+      >
+        <Text typography="headingLarge">Let's make one</Text>
+        <Text typography="headingSmall">
+          Visit the Hovalin Docs for step by step instructions
+        </Text>
+        <S.ButtonPadding>
+          <Button
+            href={routes.HOVALIN_DOCS}
+            title="Make a Hovalin"
+            onPress={() => {}}
+          />
+        </S.ButtonPadding>
+      </ContainerWithBorder>
     </div>
   </Layout>
 );
