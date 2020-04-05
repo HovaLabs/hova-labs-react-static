af0a3532ece4844c6043061b9fe13ed75adf4327
matthova <matthova87@gmail.com>
2020-04-05 09:29:24 -0700

    patched build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,19 @@
+/* eslint-disable */
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+import React from "react";
+import { DesignSystemProvider } from "@hova-labs/bento-box-web";
+import { GlobalStyle } from "./src/components";
+import { themes } from "./src/theme";
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => {
+  return (
+    <DesignSystemProvider themes={themes}>
+      <GlobalStyle />
+      {element}
+    </DesignSystemProvider>
+  );
+};
