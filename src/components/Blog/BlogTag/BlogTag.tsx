c9131b0ba69e4ea532ed6d32a2b5626c2c7bb4ae
matthova <matthova87@gmail.com>
2020-04-18 11:02:55 -0700

    patch blog tag styles

--- src/components/Blog/BlogTag/BlogTag.tsx
+++ src/components/Blog/BlogTag/BlogTag.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Text } from "@hova-labs/bento-box-web";
+import styled from "styled-components";
 import { routes } from "../../../routes";
 
+interface TextProps {
+  color: "primary" | "onBackground";
+}
+const Text = styled("div")<TextProps>`
+  ${(p) => p.theme.typography.bodyText};
+  color: ${(p) => p.theme.colors[p.color]};
+`;
 export interface BlogTag {
   onPress: () => void;
   title: string;
 }
 
 export const BlogTag: React.FC<BlogTag> = ({ onPress, title }) => {
+  const [mounted, setMounted] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
   const color =
     typeof window === "undefined" ||
-    window.location.hash.replace("#", "") === title
-      ? "onBackground"
-      : "primary";
+    window.location.hash.replace("#", "") !== title
+      ? "primary"
+      : "onBackground";
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Link to={`${routes.BLOG}#${title}`}>
-      <Text color={color} typography="bodyText" onPress={onPress}>
-        {` #${title} `}
+      <Text color={color} onClick={onPress}>
+        {`\u00A0#${title}\u00A0`}
       </Text>
     </Link>
   );
